41e5c8790d8b32bf477831a747de2be89ff2bdb1
franklinwk <franklin.wl.king@gmail.com>
2016-08-11 11:37:18 -0400

    Fixed buttons and added new endo monitor

--- Medical VR Demonstration/Assets/Scripts/ToggleModel.cs
+++ Medical VR Demonstration/Assets/Scripts/ToggleModel.cs
@@ -7,6 +7,8 @@ public class ToggleModel : VRTK.VRTK_InteractableObject {
 	public GameObject hideLabel;
 	public GameObject showLabel;
 
+	private bool hiddenStatus = false;
+
 	// Use this for initialization
 	void Start () {
 		base.Start ();
@@ -14,19 +16,28 @@ public class ToggleModel : VRTK.VRTK_InteractableObject {
 
 	public override void StartUsing(GameObject usingObject) {
 		base.StartUsing(usingObject);
-		foreach (GameObject model in thingsToHide) {
-			model.GetComponent<MeshRenderer> ().enabled = false;
-		}
-		hideLabel.GetComponent<MeshRenderer> ().enabled = false;
-		showLabel.GetComponent<MeshRenderer> ().enabled = true;
+		hiddenStatus = !hiddenStatus;
+
+		ToggleHide ();
 	}
 
-	public override void StopUsing(GameObject usingObject) {
-		base.StopUsing(usingObject);
-		foreach (GameObject model in thingsToHide) {
-			model.GetComponent<MeshRenderer> ().enabled = true;
+	public void ToggleHide() {
+		if (hiddenStatus) {
+			foreach (GameObject model in thingsToHide) {
+				model.GetComponent<MeshRenderer> ().enabled = false;
+			}
+			hideLabel.GetComponent<MeshRenderer> ().enabled = false;
+			showLabel.GetComponent<MeshRenderer> ().enabled = true;
+
+			hiddenStatus = true;
+		} else {
+			foreach (GameObject model in thingsToHide) {
+				model.GetComponent<MeshRenderer> ().enabled = true;
+			}
+			hideLabel.GetComponent<MeshRenderer> ().enabled = true;
+			showLabel.GetComponent<MeshRenderer> ().enabled = false;
+
+			hiddenStatus = false;
 		}
-		hideLabel.GetComponent<MeshRenderer> ().enabled = true;
-		showLabel.GetComponent<MeshRenderer> ().enabled = false;
 	}
 }
